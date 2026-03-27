dbf476fad00f0de6516689c1ff6cf585fa5aa6ae
mikub <miroslav.kubicek@gmail.com>
2026-03-27 13:13:48 +0100

    Make HTTP server tests use ephemeral ports

--- test/samuraibff/http/server_integration_test.clj
+++ test/samuraibff/http/server_integration_test.clj
@@ -5,10 +5,22 @@
             [clojure.test :refer :all]
             [org.httpkit.client :as http]))
 
+(defn- free-port
+  "Allocate a free local TCP port for tests.
+
+  NOTE: This does a bind-to-0, reads the selected port, then closes the socket.
+  This avoids hard-coding ports (which are frequently in use on dev machines).
+  There is a small race window, but in practice it is good enough for CI and
+  local runs."
+  []
+  (with-open [s (java.net.ServerSocket. 0)]
+    (.getLocalPort s)))
+
 (create-ns 'samuraibff)
 
 (deftest test-system-start
-  (let [config {:samuraibff/http-server {:config {:port 8080} :handler (ig/ref :samuraibff/router)}
+  (let [port (free-port)
+        config {:samuraibff/http-server {:config {:port port} :handler (ig/ref :samuraibff/router)}
                 :samuraibff/router {}}
         system (ig/init config)]
     (is (contains? system :samuraibff/http-server))
@@ -16,7 +28,8 @@
     (ig/halt! system)))
 
 (deftest test-system-restart
-  (let [config {:samuraibff/http-server {:config {:port 8081} :handler (ig/ref :samuraibff/router)}
+  (let [port (free-port)
+        config {:samuraibff/http-server {:config {:port port} :handler (ig/ref :samuraibff/router)}
                 :samuraibff/router {}}
         system (ig/init config)]
     (is (contains? system :samuraibff/http-server))
@@ -26,11 +39,12 @@
       (ig/halt! system2))))
 
 (deftest test-router-integration
-  (let [config {:samuraibff/http-server {:config {:port 8082} :handler (ig/ref :samuraibff/router)}
+  (let [port (free-port)
+        config {:samuraibff/http-server {:config {:port port} :handler (ig/ref :samuraibff/router)}
                 :samuraibff/router {}}
         system (ig/init config)]
     (is (contains? system :samuraibff/http-server))
     (is (contains? system :samuraibff/router))
-    (let [response @(http/get "http://localhost:8082/health")]
+    (let [response @(http/get (format "http://localhost:%d/health" port))]
       (is (= 200 (:status response))))
     (ig/halt! system)))
